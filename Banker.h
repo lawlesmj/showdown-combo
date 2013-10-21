b8bd468d0e1442cdd9340b9f177a4bcaf7a2100a
applegde <applegde@dukes.jmu.edu>
2013-10-21 15:41:29 -0400

    Create Banker.h

---
+++ Banker.h
@@ -0,0 +1,30 @@
+/**************************************************************************
+*                 CS-450 Fall 2013
+*
+*Programming asignment #2: Deadlock Avoidance using the Banker's Algorithm
+*
+*Written By:
+* -Douglas Applegate
+* -Mike Lawless
+*
+*Submitted on: 11/03/2013
+*
+* **************************************************************************/
+
+#ifndef BANKER_H
+#define BANKER_H
+
+#define BANK_MSGQUE_KEY 0xF7EA
+
+typedef struct {
+  long mtype; //Message purpose code 
+  struct {
+    unsigned int sender; //defined by the person who instantiates the client
+    unsigned int serialNum; //message number
+    int retAddr; // return address for the reply
+  } request;
+} msgbuf;
+
+#define NOTE_SIZE sizeof(msgbuf) - sizeof(long)
+
+#endif
