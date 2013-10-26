4916da20f50fe27856c3163aee39a758a480d1a1
applegde <applegde@dukes.jmu.edu>
2013-10-26 14:01:36 -0400

    Create client.c

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+
+int main(int argc, char *argv[]){
+
+}
