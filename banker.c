c4ad50ac3680a235b6d96148e609eb7ae9a75074
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-10-27 17:42:13 -0400

    Sorted array bullshit

--- banker.c
+++ banker.c
@@ -30,32 +30,19 @@ typedef struct {
 	int done;
 } client_info_t;
 
-void init_resources(unsigned int * numTypes, unsigned int ** available) {
-	unsigned int i;
-	FILE * file;
-	
-	file = fopen("initial.data", "r");
-	if(file == NULL) {
-		perror("fopen");
-		exit(1);
-	}
-	
-	fscanf(file, "%u", numTypes);
-	
-	*available = (unsigned int *) malloc(sizeof(unsigned int) * (*numTypes));
-	
-	if(*available == NULL) {
-		printf("Error allocating available resource vector of size %d.", *numTypes);
-		fclose(file);
-		exit(1);
-	}
-	
-	for(i = 0; i < *numTypes; i++) {
-		fscanf(file, "%u", *available + i);
-	}
-	
-	fclose(file);
-}
+typedef struct {
+	unsigned int size;
+	unsigned int items;
+	client_info_t * clientsInfo;
+} sorted_array_t;
+
+int compare_client_ids(const void* pid, const void* pelem);
+
+void add_client(sorted_array_t *, client_info_t);
+client_info_t* get_client(sorted_array_t *, int id);
+void remove_client(sorted_array_t *, int id);
+//void clear_clients(client_info_t[]);
+
 
 int main() {
 	msgbuf_t * msgbuf;
@@ -64,7 +51,7 @@ int main() {
 	key_t key;
 	
 	unsigned int * available;
-	client_info_t * clientInfo;
+	sorted_array_t clients;
 	
 	usigned int numClients = 0;
 	
@@ -72,7 +59,74 @@ int main() {
 	
 	key = BANK_MSGQUE_KEY;
 	
-	clientInfo = (client_info_t *) malloc(4 * sizeof(client_info_t))
+	clients.size = 4;
+	clients.items = 0;
+	clients.clientsInfo = (clients_info_t *) malloc(clients.size * sizeof(client_info_t))
 	
 	return 0;
+}
+
+//Put new guy sorted into the list
+void add_client(sorted_array_t * clients, client_info_t* newClient) {
+	unsigned int i;
+	
+	clients->items++;
+	// Resize if needed
+	if(clients->items > clients->size) {
+		if(clients->size == 0) clients->size = 4;
+		else				  clients->size *= 2;
+		clients->clientInfo = realloc(clients->clientInfo, clients->size * sizeof(client_info_t));
+	}
+	// shift through array until position or end is found
+	for(i = clients->items; i!=0 && clients->clientInfo[i-1]->id < newClient->id; i--) {
+		clients->clientInfo[i] = clients->clientInfo[i-1];
+	}
+	clients->clientInfo[i] = newClient;
+	
+}
+
+//Binary search for the given client
+client_info_t* get_client(sorted_array_t * clients, int id) {
+	return (client_info_t *) bsearch(&id, clients->clientInfo, 
+		clients->items, sizeof(client_info_t *),
+		compare_client_ids);
+}
+
+void remove_client(sorted_array_t * clients, int id) {
+	client_info_t * removedClient;
+	unsigned int low, high, mid;
+	
+	
+	//find the client
+	low = 0;
+	high = clients->items;
+	removedClient = NULL;
+	while(low <= high) {
+		mid = low + (high-low)/2;
+		if(id < clients->clientInfo[mid]->id)
+			high = mid - 1;
+		else if(id > clients->clientInfo[mid]->id)
+			low = mid + 1;
+		else
+			removedClient = clients->clientInfo[mid];
+			break;
+	}
+	
+	if(removedClient != NULL) {
+		clients->items--;
+		for(; mid < clients->items; mid++) {
+			clients->clientInfo[mid] = clients->clientInfo[mid + 1];
+		}
+		
+		//clean client info struct
+		free(removedClient->allocated);
+		free(removedClient->claims);
+		free(removedClient->needs);
+		//free client info struct
+		free(removedClient);
+	}
+}
+
+int compare_client_ids(const void* pid, const void* pelem) {
+	return *(int *)pid - ((client_info_t *)pelem)->id;
 }
