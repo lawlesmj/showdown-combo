875380e058483a76cda51f3c7882f8597df8f2f9
applegde <applegde@dukes.jmu.edu>
2013-10-27 14:02:45 -0400

    Update client.c

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 #include "banker.h"
 
 void randomInit(unsigned int request[]);
+void wait();
 
 //globals
 unsigned int * allocated;
@@ -23,6 +24,8 @@ int main(int argc, char *argv[]){
 	key_t key;
     //manage message info
 	int id;
+	
+	boolean safetyConcern = false;
     
     
 	init_resources(&numTypes, &claim);
@@ -32,27 +35,48 @@ int main(int argc, char *argv[]){
     
     request = (msgbuf_t *) malloc(sizeof(msgbuf_t) + sizeof(int) * (numTypes - 1));
     
-    if(request == NULL) {
-    	printf("Error allocating message buffer of size %d.", numTypes);
-	    exit(1);
+	if(request == NULL) {
+		printf("Error allocating message buffer of size %d.", numTypes);
+		exit(1);
     }
     
-    key = ftok("initial.data" , id);
+	key = ftok("initial.data" , id);
     
-    if(msgqid = msgget(key,0644| IPC_CREAT | IPC_EXCL) == -1){
-    	printf("Cannot create an exclusive message queue exiting"):
-    	exit(1);
+	if(msgqid = msgget(key,0644| IPC_CREAT | IPC_EXCL) == -1){
+		printf("Cannot create an exclusive message queue exiting"):
+		exit(1);
     }
     
-    //
-    randomInit(msgbuf->request.data[])
+    //step1 make a randomized resource request
+	randomInit(msgbuf->request.data[])
+	
+	//step2 wait for a response from the banker of a matching serial number
+	
+	//step3
+	do{
+		if(/*request granted*/){
+			
+		}
+		else if(/*request denied*/){
+			
+		}
+		else if(/*denied due to safety concerns*/){
+			
+		}
+	}while(safetyConcern == true);
+	
     
 }
 
 void randomInit(unsigned int request[]){
 	int i;
 	for(i = 0; i < numTypes; i++){
-		request[i] = ((unsigned int) (claim[i] - allocated[i] )* rand();
+		request[i] = (unsigned int) (claim[i] - allocated[i] )* rand();
 	}
 }
+
+void wait(){
+	unsigned int time = 1000 + 9000 * rand();
+	usleep(time);
+;}
 
